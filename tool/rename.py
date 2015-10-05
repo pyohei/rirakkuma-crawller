855c1ac9243f0ffa31e35c6fd446b4740e498f5b
pyohei <mukaishohei76@gmail.com>
2015-10-05 15:29:33 -0700

    Add check date function

--- tool/rename.py
+++ tool/rename.py
@@ -11,6 +11,7 @@ import time
 import re
 
 FILE_DIR = 'save'
+RENAME_PAT = '201[0-9][0-1][0-9][0-3][0-9].gif$'
 
 
 def main():
@@ -18,10 +19,8 @@ def main():
     for f in files:
         file_path = os.path.join(FILE_DIR, f)
         org_file_name = os.path.basename(file_path)
-# 正規表現チェックを
-
-
-        
+        if re.match(RENAME_PAT, org_file_name):
+            print 'Already changed file[{0}]'.format(org_file_name)
         f_stat = os.stat(file_path)
         update_epoc = f_stat.st_mtime
         update_time = time.gmtime(update_epoc)
@@ -31,7 +30,8 @@ def main():
             day=str(update_time.tm_mday).zfill(2))
         refile_path = os.path.join(FILE_DIR, file_name)
         if os.path.exists(refile_path):
-            raise OSError('Exist same file name[{0}].'.format(file_path))
+            continue
+            # raise OSError('Exist same file name[{0}].'.format(file_path))
         print file_path
 # have change?
 # Having dir?
