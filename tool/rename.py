5b072393e4decfe39be6d51e049c0e31980f3a5a
pyohei <mukaishohei76@gmail.com>
2015-10-09 00:41:12 -0700

    Make strategy

--- tool/rename.py
+++ tool/rename.py
@@ -14,16 +14,21 @@ FILE_DIR = 'save'
 BACKUP_DIR = 'rename_bk'
 RENAME_PAT = '201[0-9][0-1][0-9][0-3][0-9].gif$'
 
+# Use logging
 
 def main():
     """ main module."""
     files = os.listdir(FILE_DIR)
+    # Make backup dir
+    # Separate to module
     for f in files:
+        # Confirm as target file.
         file_path = os.path.join(FILE_DIR, f)
         org_file_name = os.path.basename(file_path)
         if re.match(RENAME_PAT, org_file_name):
             print 'Already changed file[{0}]'.format(org_file_name)
             continue
+        # Get file name
         f_stat = os.stat(file_path)
         update_epoc = f_stat.st_mtime
         update_time = time.gmtime(update_epoc)
@@ -32,17 +37,14 @@ def main():
             month=str(update_time.tm_mon).zfill(2),
             day=str(update_time.tm_mday).zfill(2))
         refile_path = os.path.join(FILE_DIR, file_name)
+        # Check file name
         if os.path.exists(refile_path):
             print 'Exist same file name[{0}].'.format(file_path)
             continue
         print file_path
-
-# have change?
-# Having dir?
-# Rename (and backup?)
-
-# How to write.
-# os.rename()
+        # Backup
+        # Rename 
+            # os.rename()
 
 if __name__ == '__main__':
     main()
